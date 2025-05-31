cc36c6f0a9ccb96dcb04178da9295fa04eceecce
maximeb <mbmsc2025@gmail.com>
2025-05-31 10:18:09 -0400

    backup before continuing

--- SlicerCART/src/SlicerCART.py
+++ SlicerCART/src/SlicerCART.py
@@ -1688,6 +1688,7 @@ class SlicerCARTWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
               return False
       return is_valid
   
+  @enter_function
   def saveNrrdSegmentation(self, currentSegmentationVersion):
         # Save .seg.nrrd file
         self.outputSegmFile = os.path.join(self.currentOutputPath,
@@ -1706,29 +1707,77 @@ class SlicerCARTWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
             msg2.buttonClicked.connect(self.msg2_clicked)
             msg2.exec()
   
+  @enter_function
+  # def saveNiiSegmentation(self, currentSegmentationVersion):
+  #       # Export segmentation to a labelmap volume
+  #       # Note to save to nifti you need to convert to labelmapVolumeNode
+  #       self.labelmapVolumeNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLLabelMapVolumeNode')
+  #       slicer.modules.segmentations.logic().ExportVisibleSegmentsToLabelmapNode(self.segmentationNode,
+  #                                                                               self.labelmapVolumeNode,
+  #                                                                               self.VolumeNode)
+  #
+  #
+  #
+  #
+  #
+  #       self.outputSegmFileNifti = os.path.join(self.currentOutputPath,
+  #                                               "{}_{}.nii.gz".format(self.currentVolumeFilename, currentSegmentationVersion))
+  #
+  #       if not os.path.isfile(self.outputSegmFileNifti):
+  #           slicer.util.saveNode(self.labelmapVolumeNode, self.outputSegmFileNifti)
+  #       else:
+  #           msg3 = qt.QMessageBox()
+  #           msg3.setWindowTitle('Save As')
+  #           msg3.setText(
+  #               f'The file {self.currentCase}_{self.annotator_name}_{self.revision_step[0]}.nii.gz already exists \n Do you want to replace the existing file?')
+  #           msg3.setIcon(qt.QMessageBox.Warning)
+  #           msg3.setStandardButtons(qt.QMessageBox.Ok | qt.QMessageBox.Cancel)
+  #           msg3.buttonClicked.connect(self.msg3_clicked)
+  #           msg3.exec()
   def saveNiiSegmentation(self, currentSegmentationVersion):
-        # Export segmentation to a labelmap volume
-        # Note to save to nifti you need to convert to labelmapVolumeNode
-        self.labelmapVolumeNode = slicer.mrmlScene.AddNewNodeByClass('vtkMRMLLabelMapVolumeNode')
-        slicer.modules.segmentations.logic().ExportVisibleSegmentsToLabelmapNode(self.segmentationNode,
-                                                                                self.labelmapVolumeNode,
-                                                                                self.VolumeNode)
+      # Step 1: Export segmentation to labelmap
+      self.labelmapVolumeNode = slicer.mrmlScene.AddNewNodeByClass(
+          'vtkMRMLLabelMapVolumeNode')
+      slicer.modules.segmentations.logic().ExportVisibleSegmentsToLabelmapNode(
+          self.segmentationNode, self.labelmapVolumeNode, self.VolumeNode
+      )
 
-        self.outputSegmFileNifti = os.path.join(self.currentOutputPath,
-                                                "{}_{}.nii.gz".format(self.currentVolumeFilename, currentSegmentationVersion))
+      # Step 2: Save to a temporary file
+      temp_path = os.path.join(slicer.app.temporaryPath, "temp_seg.nii.gz")
+      slicer.util.saveNode(self.labelmapVolumeNode, temp_path)
 
-        if not os.path.isfile(self.outputSegmFileNifti):
-            slicer.util.saveNode(self.labelmapVolumeNode, self.outputSegmFileNifti)
-        else:
-            msg3 = qt.QMessageBox()
-            msg3.setWindowTitle('Save As')
-            msg3.setText(
-                f'The file {self.currentCase}_{self.annotator_name}_{self.revision_step[0]}.nii.gz already exists \n Do you want to replace the existing file?')
-            msg3.setIcon(qt.QMessageBox.Warning)
-            msg3.setStandardButtons(qt.QMessageBox.Ok | qt.QMessageBox.Cancel)
-            msg3.buttonClicked.connect(self.msg3_clicked)
-            msg3.exec()
+      # Step 3: Load and cast to UINT8 with nibabel
+      nii = nib.load(temp_path)
+      data = nii.get_fdata().astype(np.uint8)
+      new_nii = nib.Nifti1Image(data, affine=nii.affine, header=nii.header)
+      new_nii.set_data_dtype(np.uint8)
+
+      # Step 4: Save to final path
+      self.outputSegmFileNifti = os.path.join(
+          self.currentOutputPath,
+          f"{self.currentVolumeFilename}_{currentSegmentationVersion}.nii.gz"
+      )
+
+      if not os.path.isfile(self.outputSegmFileNifti):
+          nib.save(new_nii, self.outputSegmFileNifti)
+          print(f"Saved UINT8 segmentation to: {self.outputSegmFileNifti}")
+      else:
+          msg3 = qt.QMessageBox()
+          msg3.setWindowTitle('Save As')
+          msg3.setText(
+              f'The file {self.currentCase}_{self.annotator_name}_{self.revision_step[0]}.nii.gz already exists.\nDo you want to replace the existing file?'
+          )
+          msg3.setIcon(qt.QMessageBox.Warning)
+          msg3.setStandardButtons(qt.QMessageBox.Ok | qt.QMessageBox.Cancel)
+          msg3.buttonClicked.connect(self.msg3_clicked)
+          msg3.exec()
+
+      # Step 5: Remove temp file
+      if os.path.exists(temp_path):
+          os.remove(temp_path)
+          print(f"Temporary file removed: {temp_path}")
   
+  @enter_function
   def saveSegmentationInformation(self, currentSegmentationVersion):
     # Header row
     self.previousAction = None
