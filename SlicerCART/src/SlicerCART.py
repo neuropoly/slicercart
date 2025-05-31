d138487b78e5ac5ee4c594e64001541e70f1b83a
maximeb <mbmsc2025@gmail.com>
2025-05-31 11:20:06 -0400

    before cleaning

--- SlicerCART/src/SlicerCART.py
+++ SlicerCART/src/SlicerCART.py
@@ -1742,40 +1742,96 @@ class SlicerCARTWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
           self.segmentationNode, self.labelmapVolumeNode, self.VolumeNode
       )
 
-      # Step 2: Save to a temporary file
-      temp_path = os.path.join(slicer.app.temporaryPath, "temp_seg.nii.gz")
-      slicer.util.saveNode(self.labelmapVolumeNode, temp_path)
-
-      # Step 3: Load and cast to UINT8 with nibabel
-      nii = nib.load(temp_path)
-      data = nii.get_fdata().astype(np.uint8)
-      new_nii = nib.Nifti1Image(data, affine=nii.affine, header=nii.header)
-      new_nii.set_data_dtype(np.uint8)
-
+      print('save uint8 about to')
       # Step 4: Save to final path
       self.outputSegmFileNifti = os.path.join(
           self.currentOutputPath,
           f"{self.currentVolumeFilename}_{currentSegmentationVersion}.nii.gz"
       )
+      if ConfigPath.SAVE_UINT8:
+          Debug.print(self, "Save segmentation to UINT8.")
+          # Step 2: Save to a temporary file
+          temp_path = os.path.join(slicer.app.temporaryPath, "temp_seg.nii.gz")
+          slicer.util.saveNode(self.labelmapVolumeNode, temp_path)
 
-      if not os.path.isfile(self.outputSegmFileNifti):
-          nib.save(new_nii, self.outputSegmFileNifti)
-          print(f"Saved UINT8 segmentation to: {self.outputSegmFileNifti}")
+          # Step 3: Load and cast to UINT8 with nibabel
+          nii = nib.load(temp_path)
+          data = nii.get_fdata().astype(np.uint8)
+          new_nii = nib.Nifti1Image(data, affine=nii.affine, header=nii.header)
+          new_nii.set_data_dtype(np.uint8)
+
+          # # Step 4: Save to final path
+          # self.outputSegmFileNifti = os.path.join(
+          #     self.currentOutputPath,
+          #     f"{self.currentVolumeFilename}_{currentSegmentationVersion}.nii.gz"
+          # )
+          if not os.path.isfile(self.outputSegmFileNifti):
+              nib.save(new_nii, self.outputSegmFileNifti)
+              print(f"Saved UINT8 segmentation to: {self.outputSegmFileNifti}")
+
+          # Step 5: Remove temp file
+          if os.path.exists(temp_path):
+              os.remove(temp_path)
+              print(f"Temporary file removed: {temp_path}")
       else:
-          msg3 = qt.QMessageBox()
-          msg3.setWindowTitle('Save As')
-          msg3.setText(
-              f'The file {self.currentCase}_{self.annotator_name}_{self.revision_step[0]}.nii.gz already exists.\nDo you want to replace the existing file?'
-          )
-          msg3.setIcon(qt.QMessageBox.Warning)
-          msg3.setStandardButtons(qt.QMessageBox.Ok | qt.QMessageBox.Cancel)
-          msg3.buttonClicked.connect(self.msg3_clicked)
-          msg3.exec()
+          Debug.print(self, "Save segmentation to UINT16.")
+          print()
+          if not os.path.isfile(self.outputSegmFileNifti):
+              slicer.util.saveNode(self.labelmapVolumeNode,
+                                   self.outputSegmFileNifti)
+              print(f"Saved INT16 segmentation to: {self.outputSegmFileNifti}")
 
-      # Step 5: Remove temp file
-      if os.path.exists(temp_path):
-          os.remove(temp_path)
-          print(f"Temporary file removed: {temp_path}")
+  ### working
+  # def saveNiiSegmentation(self, currentSegmentationVersion):
+  #     # Step 1: Export segmentation to labelmap
+  #     self.labelmapVolumeNode = slicer.mrmlScene.AddNewNodeByClass(
+  #         'vtkMRMLLabelMapVolumeNode')
+  #     slicer.modules.segmentations.logic().ExportVisibleSegmentsToLabelmapNode(
+  #         self.segmentationNode, self.labelmapVolumeNode, self.VolumeNode
+  #     )
+  #
+  #     print('save uint8 about to')
+  #     if ConfigPath.SAVE_UINT8:
+  #         Debug.print(self, "Save segmentation to UINT8.")
+  #         # Step 2: Save to a temporary file
+  #         temp_path = os.path.join(slicer.app.temporaryPath, "temp_seg.nii.gz")
+  #         slicer.util.saveNode(self.labelmapVolumeNode, temp_path)
+  #
+  #         # Step 3: Load and cast to UINT8 with nibabel
+  #         nii = nib.load(temp_path)
+  #         data = nii.get_fdata().astype(np.uint8)
+  #         new_nii = nib.Nifti1Image(data, affine=nii.affine, header=nii.header)
+  #         new_nii.set_data_dtype(np.uint8)
+  #
+  #         # Step 4: Save to final path
+  #         self.outputSegmFileNifti = os.path.join(
+  #             self.currentOutputPath,
+  #             f"{self.currentVolumeFilename}_{currentSegmentationVersion}.nii.gz"
+  #         )
+  #     else:
+  #         Debug.print(self, "Save segmentation to UINT16.")
+  #         print()
+  #
+  #
+  #
+  #     if not os.path.isfile(self.outputSegmFileNifti):
+  #         nib.save(new_nii, self.outputSegmFileNifti)
+  #         print(f"Saved UINT8 segmentation to: {self.outputSegmFileNifti}")
+  #     else:
+  #         msg3 = qt.QMessageBox()
+  #         msg3.setWindowTitle('Save As')
+  #         msg3.setText(
+  #             f'The file {self.currentCase}_{self.annotator_name}_{self.revision_step[0]}.nii.gz already exists.\nDo you want to replace the existing file?'
+  #         )
+  #         msg3.setIcon(qt.QMessageBox.Warning)
+  #         msg3.setStandardButtons(qt.QMessageBox.Ok | qt.QMessageBox.Cancel)
+  #         msg3.buttonClicked.connect(self.msg3_clicked)
+  #         msg3.exec()
+  #
+  #     # Step 5: Remove temp file
+  #     if os.path.exists(temp_path):
+  #         os.remove(temp_path)
+  #         print(f"Temporary file removed: {temp_path}")
   
   @enter_function
   def saveSegmentationInformation(self, currentSegmentationVersion):
