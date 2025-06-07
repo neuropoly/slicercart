ab6d9dcd522296f9a39702eca5cfa5a43a938aee
Kalum Ost <github.com.baggage052@passmail.net>
2025-06-06 21:10:46 -0400

    Deleted requirements.py, as it is no longer needed!

--- SlicerCART/src/utils/__init__.py
+++ SlicerCART/src/utils/__init__.py
@@ -1,6 +1,5 @@
 from .constants import *
 from .install_python_packages import *
-from .requirements import *
 from .debugging_helpers import *
 from .development_helpers import *
 from .ConfigPath import *
